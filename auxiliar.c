16cd4a16045ed87e2d8315170901ed4cfd2937dc
William Oliveira <william.oliveira@esig.com.br>
2023-09-09 21:46:08 -0300

    style: formata auxiliar.c

--- auxiliar.c
+++ auxiliar.c
@@ -10,7 +10,7 @@ int main()
 {
     int lA, cA, lB, cB;
     scanf("%d %d %d %d", &lA, &cA, &lB, &cB);
-    
+
     int A[lA][cA];
     srand((unsigned int)time(NULL));
     gerarMatriz(lA, cA, A);
@@ -31,22 +31,25 @@ void gerarMatriz(int linha, int coluna, int matriz[][coluna])
     {
         for (int j = 0; j < coluna; j++)
         {
-            matriz[i][j] = rand() % 101; //[0:100]
+            matriz[i][j] = rand() % 101;
         }
     }
 }
 
-int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int matriz[][coluna]) 
+int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int matriz[][coluna])
 {
     FILE *arquivo = fopen(nomeArquivo, "w");
-    if (arquivo == NULL) {
+    if (arquivo == NULL)
+    {
         printf("Erro ao abrir o arquivo");
         return 0;
     }
     fprintf(arquivo, "%d %d\n", linha, coluna);
-    for (int i = 0; i < linha; i++) {
-        for (int j = 0; j < coluna; j++) {
-            fprintf(arquivo, "%c%d%d %d\n", nomeMatriz, i+1, j+1, matriz[i][j]);
+    for (int i = 0; i < linha; i++)
+    {
+        for (int j = 0; j < coluna; j++)
+        {
+            fprintf(arquivo, "%c%d%d %d\n", nomeMatriz, i + 1, j + 1, matriz[i][j]);
         }
     }
 
