8bf7cb33273a470f0b396f96015902487d00020d
Pedro Henrique <pedrohmxavier@hotmail.com>
2023-09-22 19:42:53 -0300

    feat: implementacao da alocao dinamica no auxiliar

--- auxiliar.c
+++ auxiliar.c
@@ -3,29 +3,34 @@
 #include <time.h>
 #include <unistd.h>
 
-void gerarMatriz(int linha, int coluna, int matriz[][coluna]);
-int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int matriz[][coluna]);
+void gerarMatriz(int linha, int coluna, int **matriz);
+int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int **matriz);
+int **alocacaoDinamica(int linhas, int colunas);
+void liberarMatriz(int **matriz, int linhas);
 
 int main()
 {
     int lA, cA, lB, cB;
     scanf("%d %d %d %d", &lA, &cA, &lB, &cB);
 
-    int A[lA][cA];
+    int **A = alocacaoDinamica(lA, cA);
     srand((unsigned int)time(NULL));
     gerarMatriz(lA, cA, A);
     salvarMatriz("matrizA.txt", 'a', lA, cA, A);
 
-    int B[lB][cB];
+    int **B = alocacaoDinamica(lB, cB);
     sleep(1);
     srand((unsigned int)time(NULL));
     gerarMatriz(lB, cB, B);
     salvarMatriz("matrizB.txt", 'b', lB, cB, B);
 
+    liberarMatriz(A, lA);
+    liberarMatriz(B, lB);
+
     return 0;
 }
 
-void gerarMatriz(int linha, int coluna, int matriz[][coluna])
+void gerarMatriz(int linha, int coluna, int **matriz)
 {
     for (int i = 0; i < linha; i++)
     {
@@ -36,7 +41,7 @@ void gerarMatriz(int linha, int coluna, int matriz[][coluna])
     }
 }
 
-int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int matriz[][coluna])
+int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna, int **matriz)
 {
     FILE *arquivo = fopen(nomeArquivo, "w");
     if (arquivo == NULL)
@@ -55,4 +60,23 @@ int salvarMatriz(const char *nomeArquivo, char nomeMatriz, int linha, int coluna
 
     fclose(arquivo);
     return 0;
+}
+
+int **alocacaoDinamica(int linhas, int colunas)
+{
+    int **matriz = (int **)malloc(linhas * sizeof(int *));
+    for (int i = 0; i < linhas; i++)
+    {
+        matriz[i] = (int *)malloc(colunas * sizeof(int));
+    }
+    return matriz;
+}
+
+void liberarMatriz(int **matriz, int linhas)
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        free(matriz[i]);
+    }
+    free(matriz);
 }
